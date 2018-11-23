9616ba3eb8a7a6b3cf89d60da153f550b83a7854
Abraham¬_¬ <AbrahamRiRa>
2018-11-23 08:13:16 -0600

    Se añadio el algoritmo de ordenamiento ShellSort

--- src/AlgoritmosOrdenamiento.java
+++ src/AlgoritmosOrdenamiento.java
@@ -142,6 +142,38 @@ class MetodosOrdenamiento{
 		long fin=System.nanoTime();
 		comparador(init,fin,compas,intercambios,recorrido);
 	}
+ public void ordenShellSort(int array[]) {
+	 int salto, aux, i;
+	 int recorrido=0, intercambios=0, compas=0;
+	 boolean cambio;
+	 long init = System.nanoTime();
+	 for(salto=array.length/2;salto!=0;salto/=2) {
+		 recorrido++;
+		 cambio=true;
+		 while(cambio) {
+			 compas++;
+			 cambio=false;
+			 for(i=salto;i<array.length;i++) {
+				 
+				 if(array[i-salto]>array[i]) {
+					 aux=array[i];
+					 array[i]=array[i-salto];
+					 array[i-salto]=aux;
+					 intercambios++;
+					 cambio=true;
+				 }
+			 }
+		 }
+	 }
+	 long fin=System.nanoTime();
+	 comparador(init,fin,compas,intercambios,recorrido);
+ }
+ public void ordenQuickSort(int array[],int prim,int ult) {
+	 int i,j,pivote,aux;
+	 j=prim;
+	 i=ult;
+	 pivote=array[prim];
+ }
  
 }
 
@@ -153,25 +185,59 @@ public class AlgoritmosOrdenamiento {
 		MetodosOrdenamiento meh=new MetodosOrdenamiento();
 		int opc=0;int vector[];
 		do {
-			System.out.println("Que metodo desa utilizar"+"\n1)Burbuja"+"\n2)seleccion"+"\n3)Insercion");
+			System.out.println("Que metodo desa utilizar"+"\n1)Burbuja"+"\n2)seleccion"+"\n3)Insercion"+"\n4)Shellsort"
+		+"\n5)QuickSort");
 			opc=ent.nextInt();
 		switch(opc) {
 		case 1:
 			System.out.println("Ordenamiento Burbuja 1");
+			System.out.println("-----------------------1,000 datos----------");
 			vector=meh.llenaVector(1000);
-			meh.muestraVector(vector);
+			//meh.muestraVector(vector);
 			meh.ordenamientoBurbuja1(vector);
-			meh.muestraVector(vector);
+			//meh.muestraVector(vector);
+			System.out.println("-----------------------10,000 datos----------");
+			vector=meh.llenaVector(10000);
+			//meh.muestraVector(vector);
+			meh.ordenamientoBurbuja1(vector);
+			//meh.muestraVector(vector);
+			System.out.println("-----------------------100,000 datos----------");
+			vector=meh.llenaVector(100000);
+			//meh.muestraVector(vector);
+			meh.ordenamientoBurbuja1(vector);
+			//meh.muestraVector(vector);
 			System.out.println("Ordenamiento Burbuja 2");
+			System.out.println("-----------------------1,000 datos----------");
 			vector=meh.llenaVector(1000);
-			meh.muestraVector(vector);
+			//meh.muestraVector(vector);
 			meh.ordenamientoBurbuja2(vector);
-			meh.muestraVector(vector);
+			//meh.muestraVector(vector);
+			System.out.println("-----------------------10,000 datos----------");
+			vector=meh.llenaVector(10000);
+			//meh.muestraVector(vector);
+			meh.ordenamientoBurbuja2(vector);
+			//meh.muestraVector(vector);
+			System.out.println("-----------------------100,000 datos----------");
+			vector=meh.llenaVector(100000);
+			//meh.muestraVector(vector);
+			meh.ordenamientoBurbuja2(vector);
+			//meh.muestraVector(vector);
 			System.out.println("Ordenamiento Burbuja 3");
+			System.out.println("-----------------------1,000 datos----------");
 			vector=meh.llenaVector(1000);
-			meh.muestraVector(vector);
+			//meh.muestraVector(vector);
 			meh.ordenamientoBurbuja3(vector);
-			meh.muestraVector(vector);
+			//meh.muestraVector(vector);
+			System.out.println("-----------------------10,000 datos----------");
+			vector=meh.llenaVector(10000);
+			//meh.muestraVector(vector);
+			meh.ordenamientoBurbuja3(vector);
+			//meh.muestraVector(vector);
+			System.out.println("-----------------------100,000 datos----------");
+			vector=meh.llenaVector(100000);
+			//meh.muestraVector(vector);
+			meh.ordenamientoBurbuja3(vector);
+			//meh.muestraVector(vector);
 			break;
 		case 2:
 			System.out.println("\nOrdenamiento Seleccion");
@@ -187,13 +253,27 @@ public class AlgoritmosOrdenamiento {
 			meh.OrdenamientoInsercion(vector);
 			meh.muestraVector(vector);
 			break;
+		case 4:
+			System.out.println("\nOrdenamiento ShellSort");
+			vector=meh.llenaVector(100);
+			meh.muestraVector(vector);
+			meh.ordenShellSort(vector);
+			meh.muestraVector(vector);
+			break;
+		case 5:
+			System.out.println("\nOrdenamiento QuickSort");
+			
+			break;
+			
+		case 10:System.out.println("------Salido :v----");
+		break;
 		default:
 			System.out.println("Esa no es una opcion");
 			break;
 		}
 		
 		
-		}while(opc!=3);
+		}while(opc!=10);
 		
 
 	}
